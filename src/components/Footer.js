f58549c247702c9ab452b78ba2c55a3f621fcd39
danielwmartin1 <danielwmartin1@gmail.com>
2024-12-05 05:02:17 -0500

    Refactor footer component: adjust indentation and add space in copyright text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 import '../App.css';
 
 export default function Footer() {
-const [year] = useState(new Date().getFullYear());
+    const [year] = useState(new Date().getFullYear());
 
-return (
-    <footer>
-        <p>Daniel Martin ©{year}</p>
-    </footer>
-);
+    return (
+        <footer>
+            <p>Daniel Martin © {year}</p>
+        </footer>
+    );
 }
